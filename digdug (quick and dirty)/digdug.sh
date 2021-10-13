5ecaa557096c7c88b62a3bc15b7fedd83c6d9293
Brian Blankenship <blankcode@users.noreply.github.com>
2021-10-13 12:16:51 -0400

    Update digdug.sh
    Update

--- digdug (quick and dirty)/digdug.sh
+++ digdug (quick and dirty)/digdug.sh
@@ -1,35 +1,24 @@
-#!/bin/bash 
-#           Name: (NAME (LONG_NAME))
-#    Description: (WHAT DO I DO)
-#             By: Brian Blankenship
-#           Date: (DATE)
-#        Version: v1.0
-#
-#        Log Dir: (LOG_FILEPATH)
-#       Log File: (LOG_FILENAME)
-#
-#           MUST: (REQUIREMENTS)
-#         SHOULD: (ADVICE)
-#
-#           NOTE:
-
-#D="domain.com"; T="mx"; O="+short"; DELAY="60"; digdug;
-
-digdug() { 
-  while true; 
+## Arguments
+OPTS=$(getopt -o hstw -- "$@")
+eval set -- "$OPTS"
+while true
   do
-    NAMESERVERS=$(dig ns $D @8.8.8.8 +short);
-    [[ "$(echo -ne $NAMESERVERS|wc -w)" == "0" ]] && (
-        echo "$(date "+%Y%m%d-%H:%M:%S%z") - No Name Servers Found"
-        ) || (
-        echo -en "\n\nThe domain $D has $(echo $NAMESERVERS|wc -w) name servers.\n"; 
-        echo $NAMESERVERS; 
-        for NAMESERVER in $NAMESERVERS; 
-          do
-            echo -en "$(date "+%Y%m%d-%H:%M:%S%z") - $D at $NAMESERVER $(echo $(dig $T $D $O @$NAMESERVER))\n" &
-          done; 
-      )
-  sleep $DELAY;
-  done;
-  }
-  
+    case "$1" in
+      -h) help; shift;;
+      -s) SERVER=$2; shift 2;;
+      -t) TARGET=$2; shift 2;;
+      -w) WAIT=$2; shift 2;;
+      --) shift; break;;
+    esac; 
+done; 
+
+echo "Starting DNS-Monitor..."; 
+myDATE=$(date "+%Y%m%d-%H:%M:%S%z"); 
+
+echo "Date-Time,NS Used,host,Query Time" | tee -a DNS-Monitor-$myDATE.txt; 
+
+while true;
+do 
+	echo "$(date "+%Y%m%d-%H:%M:%S%z"),@SERVER,&TARGET,$(dig @SERVER &TARGET | grep Query | cut -d\  -f4,5)" | tee -a DNS-Monitor-$myDATE.txt; 
+	sleep $WAIT; 
+done
